7bc87afbfe823d37d1ddf293e6d636dd1ec5dd63
Soheeb <soheeb@gmail.com>
2021-05-24 17:24:40 -0500

    useState error

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as location from 'expo-location';
 
 export default function App() {
+
+  const [errorMessage, setErrorMessage] = useState(null)
+  
+  useEffect(() => {
+  load() 
+  }, [])
+
+  async function load() {
+    try {
+      let { status } = await location.requestForegroundPermissionsAsync();
+
+      if(status != 'granted') {
+        setErrorMessage('Location Access Required')
+        return
+      }
+
+    } catch (error) {
+
+    } 
+  }
+
   return (
     <View style={styles.container}>
       <Text>Coding in Production!!</Text>

d27a57006894e503212dd84ea0fba25b45290331
Soheeb <soheeb@gmail.com>
2021-05-25 16:59:59 -0500

    almost got location working

--- App.js
+++ App.js
@@ -14,22 +14,25 @@ export default function App() {
   async function load() {
     try {
       let { status } = await location.requestForegroundPermissionsAsync()
-
       if(status != 'granted') {
         setErrorMessage('Location Access Required')
+        alert(errorMessage)
         return
       }
-    const location = await location.getCurrentLocation()
+      else {
+        alert('Location permission granted. We are tracking you now, um, I mean the weather for you!')
+      }
+      const location = await Location.getCurrentPositionAsync()
 
-    const { latitude, longitude } = location.coords
-    alert('Latitude : ${latitude}, Longitude: ${longitude}')
+      const { latitude, longitude } = location.coords
+      alert('Latitude : ${latitude}, Longitude: ${longitude}')
 
-    } catch (error) {} 
+    } catch (error) { alert('error block run')} 
   }
 
   return (
     <View style={styles.container}>
-      <Text>Coding in Production!!</Text>
+      <Text>Coding in Production!</Text>
       <StatusBar style="auto" />
     </View>
   );
